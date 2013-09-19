124d990ae294bb50c14f954e51e3aa9dad410d81
kuangweizhang <kuangweizhang@gmail.com>
2013-09-19 16:51:31 -0400

    Finish 1045

--- POJ/src/p1045.java
+++ POJ/src/p1045.java
@@ -1,9 +1,29 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
 
-public class p1045 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+/**
+ * Started 9/19/2013
+ * Finished 9/19/2013
+ * @author Kevin
+ *
+ */
+public class p1045
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner cin=new Scanner(System.in);
+		double vs = cin.nextDouble();
+		double r = cin.nextDouble();
+		double c = cin.nextDouble();
+		int n = cin.nextInt();
+		
+		for(int i = 1; i <= n; i++)
+		{
+			double w = cin.nextDouble();
+			double vr = r*c*w*vs*Math.sqrt(1/(r*r*c*c*w*w + 1));
+			DecimalFormat df = new DecimalFormat("0.000");
+			System.out.println(df.format(vr));
+		}
 	}
-
 }
